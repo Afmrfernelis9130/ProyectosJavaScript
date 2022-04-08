6164db01dfb9f4c4806a2ce72503bac022641cfc
Fernelis <afmrfernelis@gmail.com>
2022-04-08 18:01:20 -0400

    Ya secambia el colo de fondo

---
+++ CambiarColores/JS/color.js
@@ -0,0 +1,27 @@
+
+
+function color(){
+
+   let element= document.getElementById("container");
+
+   element.style.background='#900';
+
+   console.log("hola")
+
+}
+
+function prueba() {
+
+    let element= document.getElementById("myID");
+
+    element.style.backgroundColor='#900';
+
+    console.log("hola")
+
+
+}
+
+function Mycolor() {
+    var element = document.getElementById("myID");
+    element.style.backgroundColor='#900';
+}
